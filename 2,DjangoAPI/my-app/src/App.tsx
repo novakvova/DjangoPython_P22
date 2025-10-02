f5032d4770afe9ee80bb565e11a093f1d963fcef
novakvova <novakvova@gmail.com>
2025-10-02 21:01:10 +0300

    Is good working app

--- 2,DjangoAPI/my-app/src/App.tsx
+++ 2,DjangoAPI/my-app/src/App.tsx
@@ -9,7 +9,7 @@ function App() {
 
     useEffect(() => {
         loadList();
-    },[]);
+    }, []);
 
     const loadList = async () => {
         try {
@@ -17,38 +17,59 @@ function App() {
                 await axios<IUserItem[]>('http://127.0.0.1:4099/api/users/');
             setUsers(result.data);
             // console.log("--Load data--", result);
-        }
-        catch (error) {
-            console.error("--Problem--",error);
+        } catch (error) {
+            console.error("--Problem--", error);
         }
     }
     // console.log("---GET USERS--", users);
 
     const contentUsers = users.map((user: IUserItem) => {
         return (
-            <tr key={user.id}>
-                <td>{user.id}</td>
-                <td>{user.last_name} {user.first_name}</td>
-                <td>{user.email}</td>
+            <tr key={user.id} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">
+                <th scope="row"
+                    className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    {user.id}
+                </th>
+                <td className="px-6 py-4">
+                    {user.last_name} {user.first_name}
+                </td>
+                <td className="px-6 py-4">
+                    {user.email}
+                </td>
+                <td className="px-6 py-4">
+
+                </td>
             </tr>
         );
     });
 
     return (
         <>
-            <h1>Привіт козаки!</h1>
-            <table>
-                <thead>
+            <h1 className={"text-3xl font-bold text-center"}>Користувачі</h1>
+            <div className="relative overflow-x-auto">
+                <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                    <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                     <tr>
-                        <th>Id</th>
-                        <th>Full name</th>
-                        <th>Email</th>
+                        <th scope="col" className="px-6 py-3">
+                            Id
+                        </th>
+                        <th scope="col" className="px-6 py-3">
+                            Full name
+                        </th>
+                        <th scope="col" className="px-6 py-3">
+                            Email
+                        </th>
+                        <th scope="col" className="px-6 py-3">
+
+                        </th>
                     </tr>
-                </thead>
-                <tbody>
+                    </thead>
+                    <tbody>
                     {contentUsers}
-                </tbody>
-            </table>
+
+                    </tbody>
+                </table>
+            </div>
         </>
     )
 }
