4f1668292e9f03423add4893b37e2561658dae83
novakvova <novakvova@gmail.com>
2025-10-25 18:55:45 +0300

    Add Identity

--- WebAPIDB/WebAPIDB/Data/AppDbContext.cs
+++ WebAPIDB/WebAPIDB/Data/AppDbContext.cs
@@ -1,13 +1,44 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using WebAPIDB.Data.Entities;
+using WebAPIDB.Data.Entities.Identity;
 
 namespace WebAPIDB.Data;
 
-public class AppDbContext : DbContext
+public class AppDbContext : IdentityDbContext<UserEntity, RoleEntity, long,
+        IdentityUserClaim<long>, UserRoleEntity, UserLoginEntity,
+        IdentityRoleClaim<long>, IdentityUserToken<long>>
 {
     public AppDbContext(DbContextOptions<AppDbContext> options)
         : base(options)
     {   
     }
+
     public DbSet<TopicEntity> Topics { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        builder.Entity<UserRoleEntity>(ur =>
+        {
+            ur.HasOne(ur => ur.Role)
+                .WithMany(r => r.UserRoles)
+                .HasForeignKey(r => r.RoleId)
+                .IsRequired();
+
+            ur.HasOne(ur => ur.User)
+                .WithMany(r => r.UserRoles)
+                .HasForeignKey(u => u.UserId)
+                .IsRequired();
+        });
+
+        builder.Entity<UserLoginEntity>(b =>
+        {
+            b.HasOne(l => l.User)
+                .WithMany(u => u.UserLogins)
+                .HasForeignKey(l => l.UserId)
+                .IsRequired();
+        });
+    }
 }
